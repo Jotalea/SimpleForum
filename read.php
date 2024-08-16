1eac76100fbe3edbea09d21c88dcca4d796b44c1
Jotalea <67925603+Jotalea@users.noreply.github.com>
2024-08-15 21:03:42 -0300

    Create read.php

---
+++ read.php
@@ -0,0 +1,41 @@
+<?php
+
+$jsonFile = './data.json';
+$data = json_decode(file_get_contents($jsonFile), true);
+
+// Get the 'id' param from URL
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+// Search for an article with the specified ID
+$article = null;
+foreach ($data as $item) {
+    if ($item['id'] === $id) {
+        $article = $item;
+        break;
+    }
+}
+
+if ($article) {
+    // Show the content
+    ?>
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo htmlspecialchars($article['title']); ?></title>
+    </head>
+    <body>
+        <h1><?php echo htmlspecialchars($article['title']); ?></h1>
+        <p><small><?php echo date("d/m/Y", $article['time']); ?></small></p>
+        <img src="<?php echo htmlspecialchars($article['image']); ?>" alt="< Article image >">
+        <p><?php echo nl2br(htmlspecialchars($article['content'])); ?></p>
+        <a href="./index.php">< go back</a>
+    </body>
+    </html>
+    <?php
+} else {
+    // Show an error
+    echo '<p>Article not found.</p>';
+}
+?>
